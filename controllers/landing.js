199a350742bf64d63a145dc966750c173dc9187d
edhale3 <edhale33@gmail.com>
2020-05-15 13:15:44 -0700

    new commit. gitignore updated

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,11 @@
 exports.get_landing= function(req,res,next){
-    res.render('landing', {title: 'Express Yeah'})
+    res.render('landing', {title: 'Chatster'})
+}
+
+
+exports.submit_chatMessage= function(req,res,next){
+    console.log(newMessage.block.h1)
+    res.render('landing', {title: 'Chatster', message: `${req.body.chat_message}`})
+    console.log("chat message:", req.body.chat_message)
+
 }
