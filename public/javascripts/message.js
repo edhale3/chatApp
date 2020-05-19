78279a1131efaf0073f46031bddeb8d68fd77519
edhale3 <edhale33@gmail.com>
2020-05-19 15:10:53 -0700

    Added function to handle AJAX delete call

---
+++ public/javascripts/message.js
@@ -0,0 +1,19 @@
+// import { DataTypes } from "sequelize/types";
+
+function deleteMessage(messageId){
+    $.ajax({
+        url: '/message/' + messageId + '/delete-json',
+        contentType:'application/json; charset=utf-8',
+        DataType: 'json',
+        data: JSON.stringify({messageId}),
+        type: 'POST',
+        success: ((result) => {
+            //replace follow button with unfollow
+            console.log("Result: ", result)
+            $("#"+ messageId).remove();
+        }),
+        error: ((error) => {
+            console.log( "Error: ", error)
+        })
+    });
+}
