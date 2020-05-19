4a67b77f8d03e910b439cf09c7387d536f5730db
edhale3 <edhale33@gmail.com>
2020-05-18 18:50:57 -0700

    Fixed constant 'models'
    Constant models didn't have 'require' and caused the create method to return undefined

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,4 +1,4 @@
-const models = ('../models')
+const models = require('../models')
 
 exports.get_landing= function(req,res,next){
     res.render('landing', {title: 'Chatster'})
@@ -6,14 +6,11 @@ exports.get_landing= function(req,res,next){
 
 
 exports.submit_chatMessage= function(req,res,next){
-    res.render('landing', {title: 'Chatster', message: `${req.body.chat_message}`})
-    console.log("chat message:", req.body.chat_message)
-    return models.Messages.create({
-        message: req.body.chat_message
+    // console.log("chat message:", req.body.chat_message)
+    return models.Message.create({
+        message: req.body.chat_message,
     }).then(message => {
+        // res.render('landing', {title: 'Chatster', message: `${req.body.chat_message}`})
         res.redirect('/');
-    });
-    // console.log("new message:", req.body.chat_message)
-    // res.redirect('/')
-
-}
+    })
+};
