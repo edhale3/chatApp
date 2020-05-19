b8d71b32fa1a78896322183aeaf1793c2572b241
edhale3 <edhale33@gmail.com>
2020-05-19 02:02:15 -0700

    Created two new routes. One for an edit page for each comment and one for updating the new comment

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,7 @@ router.get('/', landing.get_landing)
 router.post('/', landing.submit_chatMessage)
 router.get('/messages', landing.show_messages)
 router.get('/message/:message_id', landing.show_message)
+router.get('/message/:message_id/edit', landing.show_edit_message)
+router.post('/message/:message_id/edit', landing.edit_message)
 
 module.exports = router;
