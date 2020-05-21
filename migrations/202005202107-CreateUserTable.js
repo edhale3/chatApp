dd21e9e798a2f2966635b29fd715ac760719b768
edhale3 <edhale33@gmail.com>
2020-05-20 21:51:08 -0700

    Created new migration table for users

---
+++ migrations/202005202107-CreateUserTable.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize)=> {
+        return queryInterface.createTable('User', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+            },
+            username: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('User')
+    }
+};
