97c57c921695c000a7a463176d32283a26e78f55
edhale3 <edhale33@gmail.com>
2020-05-20 21:52:02 -0700

    Created User model for new users being pushed to the db

---
+++ models/User.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return User
+}
