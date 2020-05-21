68352b0f02e9f7355fb9bcf1ac630d0ede232277
edhale3 <edhale33@gmail.com>
2020-05-20 21:46:55 -0700

    Created Routes to Login and Sign Up

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 let landing = require('../controllers/landing')
+let user = require('../controllers/user')
+
+//Routes for users
+router.get('/login', user.show_login)
+router.get('/signup', user.show_signup)
+router.post('/signup', user.submit_user)
+// router.post('/login', user.login_user)
 
 /* GET home page. */
 router.get('/', landing.get_landing)
