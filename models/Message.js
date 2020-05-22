0399260eecb437ae9de1902f1735967a74bb82fb
edhale3 <edhale33@gmail.com>
2020-05-21 22:45:15 -0700

    Updated Message model

--- models/Message.js
+++ models/Message.js
@@ -16,3 +16,28 @@ module.exports = (sequelize, DataTypes) => {
 
     return Message
 }
+// to be put in User.js
+
+// 'use strict'
+
+// module.exports = (sequelize, DataTypes) => {
+//     var User = sequelize.define('User', {
+//         id: {
+//             type: DataTypes.UUID,
+//             defaultValue: DataTypes.UUIDV4,
+//             allowNull: false,
+//             primaryKey: true
+//         },
+//         username: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         }
+//     });
+
+//     return User
+// }
