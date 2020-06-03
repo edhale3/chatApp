49d2261117abaa9e7c23b0b6dfe6d17ce6b2830c
edhale3 <edhale33@gmail.com>
2020-06-02 17:36:46 -0700

    Added signup validator

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,9 @@ const passport = require('passport')
 const myPassport = require('../passport_setup')(passport);
 let flash = require('connect-flash')
 
+const { isEmpty} = require('lodash');
+const { validateUser } = require('../validators/signup');
+
 //show login page
 exports.show_login= function(req,res,next){
     res.render('user/login', {formData: {}, errors: {}})
@@ -14,6 +17,9 @@ exports.show_signup= function(req,res,next){
     res.render('user/signup', {formData:{}, errors: {} });
 }
 
+const rerender_signup= function(errors, req,res,next){
+    res.render('user/signup', {formData: req.body, errors:errors });
+}
 
 //post method for signing into the application
 exports.submit_login= function(req,res,next) {
@@ -30,16 +36,23 @@ const generateHash = function(password){
 
 //post method for adding new user
 exports.submit_user= function(req,res,next){
-    const newUser = models.Users.build({
-        email: req.body.email,
-        password: generateHash(req.body.password)
-    });
-    return newUser.save().then(result => {
-        passport.authenticate('local', {
-            successRedirect: "/",
-            failureRedirect: "/signup",
-            failureFlash: true
-        })(req,res,next)
+    let errors = {};
+    return validateUser(errors, req).then(errors => {
+        if (!isEmpty(errors)){
+            rerender_signup(errors, req,res,next);
+        } else {
+            const newUser = models.Users.build({
+                email: req.body.email,  
+                password: generateHash(req.body.password)
+            });
+            return newUser.save().then(result => {
+                passport.authenticate('local', {
+                    successRedirect: "/",
+                    failureRedirect: "/signup",
+                    failureFlash: true
+                })(req,res,next)
+            })
+        }
     })
 };
 
