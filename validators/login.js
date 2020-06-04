4804df14529c64c9ed0d7f2ab587b366e3ce7342
edhale3 <edhale33@gmail.com>
2020-06-04 02:12:45 -0700

    Cleaned up validators

--- validators/login.js
+++ validators/login.js
@@ -2,19 +2,13 @@ let models = require('../models');
 let validator = require('validator');
 let bcrypt = require("bcrypt")
 
-const validateUserFields = function(errors, req) {
-    // if(!validator.isEmail(req.body.email)){
-    //     errors["email"] = "Please use a valid email."
-    // }
-    // if(!validator.isAscii(req.body.password)) {
-    //     errors["password"] = "That's not even a valid password you idiot"
-    // }
-
-}
+// const validateUserFields = function(errors, req) {
+// 
+// }
 
 exports.validateLogin = function(errors, req) {
     return new Promise(function (resolve, reject){
-        validateUserFields(errors, req);
+        // validateUserFields(errors, req);
         return models.Users.findOne({
             where: {
                 email: req.body.email
@@ -28,26 +22,16 @@ exports.validateLogin = function(errors, req) {
                     if(res) {
                         console.log("Good job")
                     } else {
-                        console.log(req.body.password)
-                        console.log(u.password)
                         console.log("That is an incorrect password")
                         errors["password"] = "That's an incorrect password"
                     }
                     resolve(errors);
                 })
             } else {
-                errors["email"] = "Account doesn't exist"
+                errors["email"] = "Account doesn't exist. Please enter a valid email."
                 resolve(errors);
             }
 
         })
-                    // console.log(u.dataValues.password)
-                    // console.log(req.body.password)
-                    // if(u.email == null){
-                    //     errors["email"] = "WRONG ACCOUNT YOU FUCKIN RETARD"
-                    // }
-                    // if(u.password !== generateHash(req.body.password)){
-                    //     errors["password"] = "WRONG PASSWORD YOU JACK OFF"
-                    // }
     })
 }
